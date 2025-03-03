6845c32e13b2aa623866bd7d7fcc0838f5d93fdd
Alexandre Ballardin <aleballa08@gmail.com>
2025-03-03 08:55:13 -0300

    .

--- HooperStats/src/profile/profile.tsx
+++ HooperStats/src/profile/profile.tsx
@@ -1,9 +1,33 @@
 import './profile.css'
 import TopMenu from '../components/topMenu';
-import { useEffect, useState } from 'react';
 import Dashboard from './porfile_comps/dashboard';
+import { useEffect, useState } from "react";
+
 
 function Profile() {
+
+    const [lastDoneSession, setLastDoneSession] = useState<any | null>(null);
+
+    useEffect(()=>{
+        const getLastDoneSession = (): any | null => {
+            const doneSession = localStorage.getItem('doneSessions')
+    
+            if (!doneSession) return null;
+    
+            try {
+                const list: any[] = JSON.parse(doneSession)
+                return list.at(-1) || null;
+            } catch (error){
+                console.log('erro', error)
+                return null;
+            }
+        }
+        
+        setLastDoneSession(getLastDoneSession())
+    }, [])
+
+    
+
     return (
         <>
         <TopMenu/>
@@ -14,6 +38,28 @@ function Profile() {
             <div className="user_info">
                 <h1 className='username'>Username</h1>
                 <i className="fa-solid fa-user-pen"></i>
+            </div>
+            <div className='done_sessions_list'>
+                <h1>Ultima Sessão Concluída:</h1>
+                <div>
+                    <div className='done_session_exer'>
+                        {lastDoneSession ? (
+                        <div className="done_session_exer">
+                            <h3>{lastDoneSession.name || "Treino Sem Nome"}</h3>
+                            <ul>
+                                {lastDoneSession.exercises?.map((exercise: any, index: number) => (
+                                    <li key={index}>
+                                        {exercise.name} - {exercise.reps} reps - {exercise.makes} acer. - {exercise.percentage}%
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ) : (
+                        <p>Você não concluiu nenhuma sessão de treinamento.</p>
+                    )}
+                        
+                    </div>
+                </div>
             </div>
             <Dashboard></Dashboard>
         </div>
